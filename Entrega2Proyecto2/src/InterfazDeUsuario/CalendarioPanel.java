8cbe0cf2a65d4ab97ce98a8c11bcbc21c599a05f
nruip2 <n.ruizp2@uniandes.edu.co>
2023-04-26 19:30:50 -0500

    intento visualizador xd

--- Entrega2Proyecto2/src/InterfazDeUsuario/CalendarioPanel.java
+++ Entrega2Proyecto2/src/InterfazDeUsuario/CalendarioPanel.java
@@ -6,20 +6,22 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.text.DateFormatSymbols;
 import java.util.Calendar;
+
 
 import javax.swing.JPanel;
 
 public class CalendarioPanel extends JPanel {
 
-    private Calendar today;
-    private int month, year;
+    private int month;
+    private int year;
 
-    public CalendarioPanel() {
-        today = Calendar.getInstance();
-        month = today.get(Calendar.MONTH);
-        year = today.get(Calendar.YEAR);
+    public CalendarioPanel(int month, int year) {
+        
+        this.month = month;
+        this.year = year;
+
+        setSize(50, 50);
     }
 
     @Override
@@ -32,51 +34,53 @@ public class CalendarioPanel extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        // Obtener el primer y último día del mes
+    
         Calendar cal = Calendar.getInstance();
         cal.set(year, month, 1);
         int firstDayOfMonth = cal.get(Calendar.DAY_OF_WEEK);
         int lastDayOfMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
-
-        // Calcular el tamaño de las celdas
+    
         int cellSize = Math.min(getWidth() / 7, getHeight() / 9);
-
-        // Dibujar los días de la semana
+    
         g2d.setColor(Color.BLACK);
         g2d.setFont(new Font("Arial", Font.BOLD, cellSize / 2));
-        DateFormatSymbols dfs = new DateFormatSymbols();
-        String[] weekdays = dfs.getShortWeekdays();
+        String[] weekdays = {"", "D", "L", "M", "X", "J", "V", "S"};
+    
         for (int i = 1; i <= 7; i++) {
             int x = (i - 1) * cellSize;
-            int y = cellSize;
+            int y = cellSize + (cellSize / 2);
             g2d.drawString(weekdays[i].toUpperCase(), x + (cellSize / 2) - 10, y);
         }
-
-        // Dibujar los días del mes
+    
+        g2d.setColor(Color.BLACK);
+        g2d.setFont(new Font("Arial", Font.BOLD, cellSize));
+        String monthName = cal.getDisplayName(Calendar.MONTH, Calendar.LONG, getLocale());
+        int monthNameWidth = g2d.getFontMetrics().stringWidth(monthName);
+        int monthNameX = (getWidth() - monthNameWidth) / 2;
+        int monthNameY = cellSize;
+        g2d.drawString(monthName, monthNameX, monthNameY);
+    
         int day = 1;
         for (int row = 0; row < 6; row++) {
             for (int col = 0; col < 7; col++) {
                 int x = col * cellSize;
-                int y = (row + 2) * cellSize;
+                int y = (row + 3) * cellSize;
                 if (row == 0 && col < firstDayOfMonth - 1) {
-                    // Espacio en blanco antes del primer día del mes
                 } else if (day > lastDayOfMonth) {
-                    // Espacio en blanco después del último día del mes
                 } else {
-                    // Dibujar el día del mes
                     g2d.setColor(Color.BLACK);
+                    g2d.drawRect(x, y - cellSize / 2, cellSize, cellSize);
                     g2d.setFont(new Font("Arial", Font.PLAIN, cellSize / 2));
-                    g2d.drawString(String.valueOf(day), x + (cellSize / 2) - 5, y - (cellSize / 2) + 5);
+                    String dayStr = String.valueOf(day);
+                    int textWidth = g2d.getFontMetrics().stringWidth(dayStr);
+                    int textHeight = g2d.getFontMetrics().getHeight();
+                    int xOffset = (cellSize - textWidth) / 2;
+                    int yOffset = (cellSize - textHeight)/4;
+                    g2d.drawString(dayStr, x + xOffset, y - yOffset);
                     day++;
                 }
             }
         }
     }
-
-
-
-    public static void main(String[] args) {
-        new CalendarioPanel();
-    }
+    
 }
--- Entrega2Proyecto2/src/InterfazDeUsuario/ConsultarTarifas.java
+++ Entrega2Proyecto2/src/InterfazDeUsuario/ConsultarTarifas.java
@@ -1,16 +1,32 @@
 package InterfazDeUsuario;
 
+import java.awt.GridLayout;
+import java.util.Calendar;
 
 import javax.swing.JDialog;
-
 
 public class ConsultarTarifas extends JDialog{
 
     public ConsultarTarifas(){
-        add(new CalendarioPanel());
+        setLayout(new GridLayout(4,3));
 
+        Calendar cal = Calendar.getInstance();
+        int month = cal.get(Calendar.MONTH);
+        int year = cal.get(Calendar.YEAR);
 
-        setSize(500, 200);
+        for(int i=0;i<12;i++){
+            add(new CalendarioPanel(month,year));
+
+            if(month == 12){
+                month = 1;
+                year ++;
+            }
+            else{
+                month++;
+            }
+        }
+
+        setSize(500, 700);
+        setResizable(false);
     }
-    
 }
