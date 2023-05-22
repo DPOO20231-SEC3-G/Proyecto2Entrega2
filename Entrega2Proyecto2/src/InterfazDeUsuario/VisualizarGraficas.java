276906c2c6cd2399578cbf87198faa6b457fb181
nruip2 <n.ruizp2@uniandes.edu.co>
2023-05-21 19:51:29 -0500

    Visualizar

---
+++ Entrega2Proyecto2/src/InterfazDeUsuario/VisualizarGraficas.java
@@ -0,0 +1,19 @@
+package InterfazDeUsuario;
+
+import javax.swing.JDialog;
+//import javax.swing.JPanel;
+
+import java.awt.GridLayout;
+
+public class VisualizarGraficas extends JDialog{
+    
+    public VisualizarGraficas(){
+
+        setTitle("Gráficas");
+        setSize(400, 500);
+
+        setLayout(new GridLayout(1,5));
+    }
+
+    //public JPanel 
+}
